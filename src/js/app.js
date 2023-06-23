6ce9f865adc7866cc03b4ef8ddb1e4d33392eef2
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-06-23 20:05:52 +0000

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>Ligia Elizondo</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h3>San José, CR</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -54,9 +54,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://upload.wikimedia.org/wikipedia/commons/e/e1/M45map.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
