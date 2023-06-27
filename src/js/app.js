a78240e777f17db97b4960ac1c1812d172694cee
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-06-27 23:50:19 +0000

    arreglar logica de los iconos y agregar cdn

--- src/js/app.js
+++ src/js/app.js
@@ -23,27 +23,31 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables); // print on the console
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  let cover = "";
+  if (variables.includeCover) {
+    cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  } else {
+    cover = "<div class='cover'></div>";
+  }
+
+  // Reset the website body with the new HTML output
+  document.querySelector("#widget_content").innerHTML = `
+        <div class="widget">
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Ligia Elizondo</h1>
-          <h2>Web Developer</h2>
-          <h3>San José, CR</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
-    `;
+      `;
 }
 
 /**
@@ -57,7 +61,8 @@ window.onload = function() {
     background:
       "https://upload.wikimedia.org/wikipedia/commons/e/e1/M45map.jpg",
     // this is the url for the profile avatar
-    avatarURL: "",
+    avatarURL:
+      "https://scontent.fsyq2-1.fna.fbcdn.net/v/t1.6435-9/96520446_3031975536862458_7506318977488912384_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_ohc=AorQEprDuSoAX_FNVhm&_nc_ht=scontent.fsyq2-1.fna&oh=00_AfCu4BCEsERFLU3Wy-Wz1-MY0sUBVD6xQsFcZlDUfrl0rg&oe=64BD4C95",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
